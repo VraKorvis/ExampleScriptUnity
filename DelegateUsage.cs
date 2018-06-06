2a5ac42fd2f0e759de949d86ac9af0c7c1f1da88
VraKorvis <it.korvis@gmail.com>
2018-06-06 15:30:11 +0400

    add Example event delegate

---
+++ DelegateUsage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateUsage : MonoBehaviour {
+
+    public delegate void EventHandler(int Param1, int Param2);
+
+    public event EventHandler EH;
+
+    ///<summary>
+    /// добавление делегата HandleMyEvent 
+    /// </summary>
+
+    private void Awake() {
+        // добавить обработчик события в список делегатов
+        EH += HandleMyEvent;
+    }
+
+    private void Start() {
+        EH(0,0);
+    }
+
+    ///<summary>
+    /// пример обработчика события. Позволяет ссылаться на него как на делегат типа EventHandler
+    /// </summary>
+
+    private void HandleMyEvent(int Param1, int Param2) {
+        Debug.Log("Event Called");
+    }
+}
